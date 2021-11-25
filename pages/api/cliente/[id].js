bd2d08d02d7b01c224e2447b272871ba34b30681
Lucas Delmou <lucasdelmartins@gmail.com>
2021-11-25 08:24:00 -0300

    msg de erro

--- pages/api/cliente/[id].js
+++ pages/api/cliente/[id].js
@@ -17,7 +17,7 @@ const handler = async(req, res) => {
 
                 return res.status(200).json({success: true, cliente: cliente});
             }catch(error){
-                return res.status(400).json({success: false, message: `Falha ao cadastrar cliente! ${error}`});
+                return res.status(400).json({success: false, message: `Falha ao obter cliente! ${error}`});
             }
             break;
         case 'PUT':
