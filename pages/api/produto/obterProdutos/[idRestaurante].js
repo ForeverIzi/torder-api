a683256efbce4f9d3ef85562fb90c4b47de9a163
Lucas Delmou <lucasdelmartins@gmail.com>
2021-11-26 18:55:17 -0300

    Pf não morra

--- pages/api/produto/obterProdutos/[idRestaurante].js
+++ pages/api/produto/obterProdutos/[idRestaurante].js
@@ -1,5 +1,6 @@
 import dbConnect from "../../../../utils/dbConnect"; 
 import Produto from '../../../../models/Produto';
+import Produto from '../../../../models/Categoria';
 import middlewareAuth from '../../../../middlewares/auth';
 
 
@@ -13,9 +14,9 @@ const handler = async(req, res) => {
             try{
                 let produto;
                 if(categoria)
-                     produto = await Produto.find({restaurante: idRestaurante, categoria: categoria});
+                     produto = await Produto.find({restaurante: idRestaurante, categoria: categoria}).populate({ path: 'categoria', model: Categoria });
                 else
-                    produto = await Produto.find({restaurante: idRestaurante});
+                    produto = await Produto.find({restaurante: idRestaurante}).populate({ path: 'categoria', model: Categoria });
                 
                 if(!produto){
                     return res.status(400).json({success: false});
