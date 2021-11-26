1548474ae15788c3558bea8d5b8c91b8edd3c4ab
Lucas Delmou <lucasdelmartins@gmail.com>
2021-11-26 12:01:55 -0300

    Produto retornando com categoria ou sem

--- pages/api/produto/obterProdutos/[idRestaurante].js
+++ pages/api/produto/obterProdutos/[idRestaurante].js
@@ -12,7 +12,12 @@ const handler = async(req, res) => {
     switch(method){
         case 'GET':
             try{
-                const produto = await Produto.find({restaurante: idRestaurante, categoria: categoria});
+                let produto;
+                if(categoria)
+                     produto = await Produto.find({restaurante: idRestaurante, categoria: categoria});
+                else
+                    produto = await Produto.find({restaurante: idRestaurante});
+                
                 if(!produto){
                     return res.status(400).json({success: false});
                 }
