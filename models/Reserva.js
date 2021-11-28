f5cf90c06358d723db2ea07e466cbd185acbe25c
Lucas Delmou <lucasdelmartins@gmail.com>
2021-11-28 20:56:56 -0300

    mesma coisa que o ultimo comit

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const ReservaSchema = new mongoose.Schema({
     horarioCiacao:{
@@ -8,6 +9,8 @@ const ReservaSchema = new mongoose.Schema({
     horarioReserva: {
         type: Date,
     },
+    status: {
+        type: String},
     mesa:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Mesa',
@@ -17,5 +20,12 @@ const ReservaSchema = new mongoose.Schema({
         ref: 'Cliente',
     }
 })
+
+ReservaSchema.pre('save', function(next){
+    const data = Date.now();
+    this.horarioReserva = moment(data).add(30, 'm').toDate();
+    console.log(this.horarioReserva);
+    next();
+});
 
 module.exports = mongoose.models.Reserva || mongoose.model('Reserva',ReservaSchema);
