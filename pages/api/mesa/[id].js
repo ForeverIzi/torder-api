2dee91adce7e9506211a254f9303816d3942fa3f
Lucas Delmou <lucasdelmartins@gmail.com>
2021-11-28 18:04:01 -0300

    error consertado

--- pages/api/mesa/[id].js
+++ pages/api/mesa/[id].js
@@ -12,7 +12,6 @@ const handler = async(req, res) => {
     switch(method){
         case 'GET':
             try{
-                console.log(id);
                 const mesa = [await Mesa.findById(id).populate({ path: 'restaurante', model: Restaurante })];
                 if(!mesa){
                     return res.status(400).json({success: false});
@@ -29,7 +28,6 @@ const handler = async(req, res) => {
                     new: true,
                     runValidators: true
                 });
-
                 if(!mesa){
                     return res.status(400).json({success: false, message: `Falha ao atualizar mesa!`});
                 }
