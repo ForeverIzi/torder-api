7ae39675ba6eb83e679c58955f7a761cd9552d3c
Lucas Delmou <lucasdelmartins@gmail.com>
2021-11-28 11:36:27 -0300

    erro ridiculo dos ids

--- pages/api/mesa/[id].js
+++ pages/api/mesa/[id].js
@@ -12,7 +12,8 @@ const handler = async(req, res) => {
     switch(method){
         case 'GET':
             try{
-                const mesa = [await Mesa.findById({id}).populate({ path: 'restaurante', model: Restaurante })];
+                console.log(id);
+                const mesa = [await Mesa.findById(id).populate({ path: 'restaurante', model: Restaurante })];
                 if(!mesa){
                     return res.status(400).json({success: false});
                 }
