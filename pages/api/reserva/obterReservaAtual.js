42b886edde0df0620a545ce76c32be31469793f9
Lucas Delmou <lucasdelmartins@gmail.com>
2021-11-28 22:08:49 -0300

    Obter Reserva Atual

---
+++ pages/api/reserva/obterReservaAtual.js
@@ -0,0 +1,29 @@
+import dbConnect from "../../../utils/dbConnect"; 
+import Reserva from '../../../models/Reserva';
+import Mesa from "../../../models/Mesa";
+import middlewareAuth from '../../../middlewares/auth';
+
+dbConnect();
+
+const handler = async(req, res) => {
+    const{ query: { idCliente }, method } = req;
+
+    switch(method){
+        case 'GET':
+            try{
+                const reserva = await Reserva.find({cliente: idCliente}).where('status').equals('Reservado').populate({ path: 'mesa', model: Mesa });
+                if(!reserva){
+                    return res.status(400).json({success: false});
+                }
+
+                return res.status(200).json({success: true, reserva: reserva});
+            }catch(error){
+                return res.status(400).json({success: false, message: `Falha ao obter reserva! ${error}`});
+            }
+            break;
+        default:
+            return res.status(400).json({success: false, message: "Requisição inválida"});
+            break;
+    }   
+}
+export default middlewareAuth(handler);
